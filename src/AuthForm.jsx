e183627be5ce9688d185754a4aeabca119a47620
ArthurNovember <matej.artur.novacek@email.cz>
2025-07-31 18:41:52 +0200

    login

--- src/AuthForm.jsx
+++ src/AuthForm.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./AuthForm.css";
 import { Link } from "react-router-dom";
+
+
+
 const AuthForm = () => {
+ const [isSignUp,setIsSignUp]=useState(true);
+ const [isLogin,setIsLogin]=useState(false);
   return (
    <div>
    <div className="FormContainer">
-          <button className="formStyleA" >SIGN UP</button>
-          <button className="formStyleB">LOG IN</button>
+          <button className="formStyleA" onClick={()=>{setIsSignUp(true); setIsLogin(false)}}>SIGN UP</button>
+          <button className="formStyleB" onClick={()=>{setIsLogin(true); setIsSignUp(false)}}>LOG IN</button>
+        
+        {isSignUp && (
+        <div className="signOption">
           <div className="labelWithInputs">
         <div className="labelWithInput">
           <label>Username:</label>
@@ -18,18 +26,36 @@ const AuthForm = () => {
          </div>
         <div className="labelWithInput">
           <label>Password:</label>
-          <input className="formInput"></input>
+          <input className="formInput" type="password"></input>
           </div>
          
           <div className="labelWithInput">
           <label>Confirm Password:</label>
-          <input className="formInput"></input>
+          <input className="formInput" type="password"></input>
           </div>
           </div>
           <button className="signup">SIGN UP</button>
-    </div>
-    
+    </div>)}
 
+
+            {isLogin && (
+        <div className="signOption">
+          <div className="labelWithInputs">
+
+          <div className="labelWithInput">
+          <label>Email:</label>
+          <input className="formInput"></input>
+         </div>
+        <div className="labelWithInput">
+          <label>Password:</label>
+          <input className="formInput" type="password"></input>
+          </div>
+
+          </div>
+          <button className="signup">LOG IN</button>
+    </div>)}
+    
+</div>
     
     </div>
   );

2b7f08adf66a7d94f8258a4fed6d63ff6f947c60
ArthurNovember <matej.artur.novacek@email.cz>
2025-08-14 15:34:51 +0200

    authfix

--- src/AuthForm.jsx
+++ src/AuthForm.jsx
@@ -73,7 +73,7 @@ const AuthForm = ({ onLoginSuccess }) => {
   };
 
   return (
-    <div>
+    <div className="authfull">
       <div className="FormContainer">
         <button
           style={{
